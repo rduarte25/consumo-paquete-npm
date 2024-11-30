cb72993b7ae2072c4e51c733bbe9ea26243d61f5
Rafael Duarte <elyouus94@gmail.com>
2024-11-30 13:00:22 -0400

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
 import "./App.css";
-import {
-  ProductCard,
-  ProductImage,
-  ProductTitle,
-  ProductButtons,
-} from "radm-product-card";
+import { Label } from "rduarte-storybook-components";
 
 function App() {
-  const product = {
-    id: "1",
-    title: "Coffee Mug - Card",
-    image: "./coffee-mug.png",
-  };
   return (
     <div className="App App-header">
-      <ProductCard
-        product={product}
-        initialValue={{
-          count: 4,
-          maxCount: 10,
-        }}
-      >
-        {({
-          handleReset,
-          handleIncrease,
-          count,
-          isMaxCountReached,
-          maxCount,
-        }) => (
-          <>
-            <ProductImage image={product.image} />
-            <ProductTitle title={product.title} />
-            <ProductButtons />
-            {count}
-            <button onClick={() => handleReset()}>Reset</button>
-          </>
-        )}
-      </ProductCard>
+      <Label label={'Consumiendo etiqueta'} size={'h1'} color={'secondary'}/>
     </div>
   );
 }
